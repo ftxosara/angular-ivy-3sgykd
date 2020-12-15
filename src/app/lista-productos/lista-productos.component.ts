3a6a5229b2059d5af1571ec0a23b86ad9debaa4a
ftxosara <ftxosara@hotmail.com>
2020-12-15 15:36:45 -0500

    1 file modified

--- src/app/lista-productos/lista-productos.component.ts
+++ src/app/lista-productos/lista-productos.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { Component } from '../productos';
+import { productos } from '../productos';
 
 @Component({
-  selector: 'app-lista-productos',
-  templateUrl: './lista-productos.component.html',
-  styleUrls: ['./lista-productos.component.css']
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
-export class listaproductosComponent{
-productos=productos;
+export class ProductListComponent {
+  productos = productos;
 
-share() {
-  window.alert('el producto ha sido compartido');
+  share() {
+    window.alert('The product has been shared!');
+  }
 }
 
-}
